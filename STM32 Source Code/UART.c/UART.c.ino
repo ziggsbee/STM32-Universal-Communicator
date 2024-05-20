c601914926cebc6d734834337a0ed70c13e40cef
Nick Porsch <porschnickalas@gmail.com>
2024-05-20 18:38:39 -0500

    test baud done, working on pass thru.

--- STM32 Source Code/UART.c/UART.c.ino
+++ STM32 Source Code/UART.c/UART.c.ino
@@ -1,6 +1,6 @@
 /*
   UART Tester
-  Using a STM32 WROOM board this script is used to determine the baud rate of a target device connected to GPIO 10 - Rx and GPIO 11 - Tx.
+  Using a STM32 WROOM board this script is used to determine the baud rate of a target device connected to GPIO 22 - Rx and GPIO 23 - Tx.
   Please ensure the ground of the STM32 is also connected to your target device.
 */
 
@@ -10,7 +10,7 @@ enum states {
 
 int baud_rates[] = {4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600};
 
-int state = PROBE;
+int state = WAITING;
 
 void setup()
 {
@@ -19,23 +19,73 @@ void setup()
 }
 
 
+void getState(){
+        Serial.println("Please select a new State:");
+        Serial.println("1: Probe for baud rate - A list of popular baud rates will be used to test the target for the valid baud rate. \nYou may also specify a specific baud rate to test for.");
+        Serial.println("2: UART Pass Though. Please specify a baud rate you would like to test and connect the device to pins");
+  
+        while (!Serial.available());
+        int userInput = Serial.read();
+        while (Serial.available() > 0 && Serial.read() != '\n');
+  
+        if (userInput == '1'){
+          state = PROBE;
+        }
+        else if (userInput == '2'){
+          state = PASS_THRU;
+        }
+        else{
+          Serial.println("Invalid Option.");
+        }
+  
+}
 
-void loop() {
+void statePassThru(){
+      Serial.println("Please enter the Baud Rate: ");
+          
+      while (!Serial.available());
+          
+      int userInput = Serial.parseInt();
+      while (Serial.available() > 0 && Serial.read() != '\n');
+      int baudRate = userInput;
+      Serial2.begin(baudRate, SERIAL_8N1, 22,23);
+      Serial2.setTimeout(300);
+      while(true){
+        int bytesRead = 0;
+  
+        while (bytesRead < 1){
+          if(Serial2.available() > 0) {
+            char c = Serial2.read();
+            Serial.print(c);
+          }
+        }
+      }
+}
 
-
-  switch (state)
+void testSerial(int baudRate){
+  Serial2.begin(baudRate, SERIAL_8N1, 22,23);
+  Serial2.setTimeout(300);
+  char targetOutput[101];
+  int bytesRead = 0;
+  while (bytesRead < 100){
+      if(Serial2.available() > 0) {
+        char c = Serial2.read();
+        targetOutput[bytesRead++] = c;
+        }
+  }
+  Serial.print("Output: ");
+  Serial.print(baudRate);
+  Serial.print(": ");
+  Serial.println(targetOutput);
+  
+  Serial2.end();
+  while (Serial2.available() > 0)
   {
-    case WAITING:
-      Serial.println("Please select a new State:");
-      Serial.println("1: Probe for baud rate - A list of popular baud rates will be used to test the target for the valid baud rate. \nYou may also specify a specific baud rate to test for.");
-      Serial.println("2: UART Pass Though. Please specify a baud rate you would like to test and connect the device to pins");
-      break;
+    Serial2.read();
+  }
+}
 
-    case PASS_THRU:
-      Serial.println("PASS THRU");
-      break;
-
-    case PROBE:
+void stateProbe(){
       Serial.println("Would you like to test a custom baud rate? (Y/N): ");
       
       while (!Serial.available());
@@ -52,7 +102,7 @@ void loop() {
       }
       int customBaud = 0;
       if( userInput == 'Y' || userInput == 'y'){
-            Serial.println("Please enter the Baud Rate: ");
+           Serial.println("Please enter the Baud Rate: ");
           
           while (!Serial.available());
           
@@ -62,33 +112,43 @@ void loop() {
 
           customBaud = userInput;
 
-//We were able to get the custom baudrate so far. Need to work on the below code to actually get the 2nd serial connection working.
-          Serial2.begin(customBaud, SERIAL_8N1, 10,11);
-          Serial2.setTimeout(300);
-          char targetOutput[101];
-          
-          int bytesRead = 0;
-
-          while (bytesRead < 100){
-            if(Serial2.available() > 0) {
-              char c = Serial2.read();
-              targetOutput[bytesRead++] = c;
-              
-            }
-          }
-
-          Serial2.print("Output: ");
-          Serial.println(targetOutput);
         
       }
       else {
+        Serial.println("Testing standard baud rates");
+        int baudIndex = 0;
         
-      }
 
+        while (baudIndex < (sizeof(baud_rates)/ sizeof(int))) {
+          customBaud = baud_rates[baudIndex];
+          testSerial(customBaud);
+          baudIndex++;
+        }
+      }
+      state = WAITING;
+}
+
+
+void loop() {
+
+char userInput = ' ';
+
+  switch (state)
+  {
+    case WAITING:
+      getState();
+    break;
+
+    case PASS_THRU:
+      statePassThru();
       break;
 
-
-
+    case PROBE:
+      stateProbe();
+      
+      break;
   }
+
+  
 
 }
